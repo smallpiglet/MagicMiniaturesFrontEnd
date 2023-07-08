8c4f9c65af4bf6014b09afd43239d3e60708013e
SmallPiglet <20598@gapps.ispu.ru>
2023-07-08 19:04:30 +0300

    create app

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/app.css"
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    )
+  }
+}
+
+export default App;
